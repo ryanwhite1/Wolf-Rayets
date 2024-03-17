a020af29a24b3457131b82ce4247d3d574f25116
Ryan White <ryanwhite_1@yahoo.com.au>
2024-03-17 19:16:32 +1000

    Update WR_GM_GUI.py

--- WR_GM_GUI.py
+++ WR_GM_GUI.py
@@ -80,51 +80,74 @@ def update_frequency(param, new_val):
     canvas.draw()
 
 
-ecc = tkinter.Scale(root, from_=0, to=0.99, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('eccentricity', v), label="Eccentricity", resolution=0.01)
-inc = tkinter.Scale(root, from_=0, to=180, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('inclination', v), label="Inclination", resolution=0.1)
+ecc = tkinter.Scale(root, from_=0, to=0.99, orient=tkinter.HORIZONTAL, 
+                    command=lambda v: update_frequency('eccentricity', v), label="Eccentricity", resolution=0.01)
+ecc.set(starcopy['eccentricity'])
+inc = tkinter.Scale(root, from_=0, to=180, orient=tkinter.HORIZONTAL, 
+                    command=lambda v: update_frequency('inclination', v), label="Inclination", resolution=0.1)
+inc.set(starcopy['inclination'])
 asc_node = tkinter.Scale(root, from_=0, to=360, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('asc_node', v), label="Ascending Node", resolution=0.1)
+                         command=lambda v: update_frequency('asc_node', v), label="Ascending Node", resolution=0.1)
+asc_node.set(starcopy['asc_node'])
 arg_peri = tkinter.Scale(root, from_=0, to=180, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('arg_peri', v), label="Argument of Periastron", resolution=0.1)
+                         command=lambda v: update_frequency('arg_peri', v), label="Argument of Periastron", resolution=0.1)
+arg_peri.set(starcopy['arg_peri'])
 opang = tkinter.Scale(root, from_=0, to=180, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('open_angle', v), label="Open Angle", resolution=0.1)
+                      command=lambda v: update_frequency('open_angle', v), label="Open Angle", resolution=0.1)
+opang.set(starcopy['open_angle'])
 m1 = tkinter.Scale(root, from_=5, to=50, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('m1', v), label="WR Mass", resolution=0.1)
-m2 = tkinter.Scale(root, from_=5, to=50, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('m2', v), label="Companion Mass", resolution=0.1)
-phase = tkinter.Scale(root, from_=0., to=1.5, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('phase', v), label="Phase", resolution=0.01)
+                   command=lambda v: update_frequency('m1', v), label="WR Mass", resolution=0.1)
+m1.set(starcopy['m1'])
+m2 = tkinter.Scale(root, from_=5, to=50, orient=tkinter.HORIZONTAL, 
+                   command=lambda v: update_frequency('m2', v), label="Companion Mass", resolution=0.1)
+m2.set(starcopy['m2'])
+phase = tkinter.Scale(root, from_=0., to=1.5, orient=tkinter.HORIZONTAL, 
+                      command=lambda v: update_frequency('phase', v), label="Phase", resolution=0.01)
+phase.set(starcopy['phase'])
 n_orb = tkinter.Scale(root, from_=1, to=2, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('n_orbits', v), label="Shells")
+                      command=lambda v: update_frequency('n_orbits', v), label="Shells")
+n_orb.set(starcopy['n_orbits'])
 turnon = tkinter.Scale(root, from_=-180, to=0, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('turn_on', v), label="Turn On Angle", resolution=0.1)
+                       command=lambda v: update_frequency('turn_on', v), label="Turn On Angle", resolution=0.1)
+turnon.set(starcopy['turn_on'])
 turnoff = tkinter.Scale(root, from_=0, to=180, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('turn_off', v), label="Turn Off Angle", resolution=0.1)
+                        command=lambda v: update_frequency('turn_off', v), label="Turn Off Angle", resolution=0.1)
+turnoff.set(starcopy['turn_off'])
 distance = tkinter.Scale(root, from_=1e3, to=1e4, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('distance', v), label="Distance (pc)", resolution=1)
+                         command=lambda v: update_frequency('distance', v), label="Distance (pc)", resolution=1)
+distance.set(starcopy['distance'])
 ws1 = tkinter.Scale(root, from_=0, to=5e3, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('windspeed1', v), label="WR Windspeed (km/s)", resolution=1)
+                    command=lambda v: update_frequency('windspeed1', v), label="WR Windspeed (km/s)", resolution=1)
+ws1.set(starcopy['windspeed1'])
 ws2 = tkinter.Scale(root, from_=0, to=5e3, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('windspeed2', v), label="Companion Windspeed (km/s)", resolution=1)
+                    command=lambda v: update_frequency('windspeed2', v), label="Companion Windspeed (km/s)", resolution=1)
+ws2.set(starcopy['windspeed2'])
 period = tkinter.Scale(root, from_=0, to=200, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('period', v), label="Period (yr)", resolution=0.1)
+                       command=lambda v: update_frequency('period', v), label="Period (yr)", resolution=0.1)
+period.set(starcopy['period'])
 osd = tkinter.Scale(root, from_=0, to=180, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('orb_sd', v), label="Orb. Var. SD (deg)", resolution=0.1)
+                    command=lambda v: update_frequency('orb_sd', v), label="Orb. Var. SD (deg)", resolution=0.1)
+osd.set(starcopy['orb_sd'])
 oamp = tkinter.Scale(root, from_=0, to=3, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('orb_amp', v), label="Orb. Var. Amp", resolution=0.01)
+                     command=lambda v: update_frequency('orb_amp', v), label="Orb. Var. Amp", resolution=0.01)
+oamp.set(starcopy['orb_amp'])
 azsd = tkinter.Scale(root, from_=0, to=180, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('az_sd', v), label="Az. Var. SD (deg)", resolution=0.1)
+                     command=lambda v: update_frequency('az_sd', v), label="Az. Var. SD (deg)", resolution=0.1)
+azsd.set(starcopy['az_sd'])
 azamp = tkinter.Scale(root, from_=0, to=3, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('az_amp', v), label="Az. Var. Amp", resolution=0.01)
+                      command=lambda v: update_frequency('az_amp', v), label="Az. Var. Amp", resolution=0.01)
+azamp.set(starcopy['az_amp'])
 histmax = tkinter.Scale(root, from_=1, to=0, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('histmax', v), label="Max Brightness", resolution=0.01)
+                        command=lambda v: update_frequency('histmax', v), label="Max Brightness", resolution=0.01)
+histmax.set(starcopy['histmax'])
 sigma = tkinter.Scale(root, from_=0.01, to=10, orient=tkinter.HORIZONTAL,
-                              command=lambda v: update_frequency('sigma', v), label="Gaussian Blur", resolution=0.01)
+                      command=lambda v: update_frequency('sigma', v), label="Gaussian Blur", resolution=0.01)
+sigma.set(starcopy['sigma'])
 
 sliders = [ecc, inc, phase, m1, opang, m2, turnon, asc_node, turnoff, distance, arg_peri, n_orb, ws1, period, ws2,
            osd, histmax, oamp, azsd, sigma, azamp]
+
+
 # Packing order is important. Widgets are processed sequentially and if there
 # is no space left, because the window is too small, they are not displayed.
 # The canvas is rather flexible in its size, so we pack it last which makes
