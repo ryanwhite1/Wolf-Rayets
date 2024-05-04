Partial gradient fix

WR_GM_MCMC.py
@@ -26,6 +26,7 @@ apep = wrb.apep.copy()
 particles, weights = gm.dust_plume(wrb.apep)
     
 X, Y, H = gm.smooth_histogram2d(particles, weights, wrb.apep)
+# X, Y, H = gm.spiral_grid(particles, weights, wrb.apep)
 obs_err = 0.01 * np.max(H)
 H += np.random.normal(0, obs_err, H.shape)
 gm.plot_spiral(X, Y, H)
@@ -327,24 +328,24 @@ init_params = apep.copy()
 #                               num_chains=1,
 #                               num_samples=300,
 #                               num_warmup=20,
-#                               progress_bar=True)
+# #                               progress_bar=True)
 # sampler = numpyro.infer.MCMC(numpyro.infer.HMC(apep_model, 
 #                                                 target_accept_prob=0.3,
 #                                                 init_strategy=numpyro.infer.initialization.init_to_value(values=init_params),
 #                                                 forward_mode_differentiation=True,
-#                                                 step_size=1e-7),
+#                                                 step_size=1e-3),
 #                               num_chains=1,
 #                               num_samples=300,
 #                               num_warmup=20,
 #                               progress_bar=True)
-# # sampler = numpyro.infer.MCMC(numpyro.infer.NUTS(apep_model),
-# #                               num_chains=1,
-# #                               num_samples=300,
-# #                               num_warmup=20)
-# # sampler = numpyro.infer.MCMC(numpyro.infer.SA(apep_model, init_strategy=numpyro.infer.initialization.init_to_value(values=init_params)),
-# #                               num_chains=num_chains,
-# #                               num_samples=2000,
-# #                               num_warmup=1000)
+# # # sampler = numpyro.infer.MCMC(numpyro.infer.NUTS(apep_model),
+# # #                               num_chains=1,
+# # #                               num_samples=300,
+# # #                               num_warmup=20)
+# # # sampler = numpyro.infer.MCMC(numpyro.infer.SA(apep_model, init_strategy=numpyro.infer.initialization.init_to_value(values=init_params)),
+# # #                               num_chains=num_chains,
+# # #                               num_samples=2000,
+# # #                               num_warmup=1000)
 # sampler.run(jax.random.PRNGKey(1), obs, obs_err)
 
 # results = sampler.get_samples()
@@ -363,10 +364,10 @@ init_params = apep.copy()
 
 # a = jax.jit(numpyro.infer.util.potential_energy(apep_model, (obs, obs_err), {}, {"eccentricity":0.7}))
 # @jit
-def a(e):
-    blah = numpyro.infer.util.log_likelihood(apep_model, {"eccentricity":e}, obs, obs_err)
-    print(blah['y'])
-    return blah
+# def a(e):
+#     blah = numpyro.infer.util.log_likelihood(apep_model, {"eccentricity":e}, obs, obs_err)
+#     print(blah['y'])
+#     return blah
 
 
 
@@ -375,6 +376,7 @@ def man_loglike(e):
     starcopy['eccentricity'] = e
     samp_particles, samp_weights = gm.dust_plume(starcopy)
     _, _, samp_H = gm.smooth_histogram2d(samp_particles, samp_weights, starcopy)
+    # _, _, samp_H = gm.spiral_grid(samp_particles, samp_weights, starcopy)
     samp_H = samp_H.flatten()
     
     return -0.5 * jnp.sum(jnp.square((samp_H - obs) / obs_err))
@@ -383,6 +385,8 @@ def man_loglike(e):
     
 like = jit(vmap(jax.value_and_grad(man_loglike)))
 
+
+
 n = 50
 numpyro_logLike = np.zeros(n)
 manual_logLike = np.zeros(n)
@@ -390,11 +394,11 @@ eccentricities = np.linspace(0, 0.9, n)
 de = eccentricities[1] - eccentricities[0]
 
 
-# for i in range(n):
-#     print("hello world :)")
-#     t1 = time.time()
-#     manual_logLike[i] = man_loglike(eccentricities[i])
-#     print(time.time() - t1)
+# # for i in range(n):
+# #     print("hello world :)")
+# #     t1 = time.time()
+# #     manual_logLike[i] = man_loglike(eccentricities[i])
+# #     print(time.time() - t1)
     
 vals, grads = like(eccentricities)
 
@@ -406,6 +410,7 @@ ax1.plot(eccentricities, vals)
 ax1.axvline(apep['eccentricity'])
 ax2.plot(eccentricities, grads)
 ax2.plot(eccentricities, np.gradient(vals, de))
+# ax2.plot(eccentricities, -np.gradient(np.gradient(vals)))
 ax2.axvline(apep['eccentricity'])
 ax2.axhline(0)
     

WR_Geom_Model.py
@@ -402,26 +402,36 @@ def smooth_histogram2d(particles, weights, stardata):
     _, xedges, yedges = jnp.histogram2d(x, y, bins=im_size, weights=weights, range=jnp.array([[-bound, bound], [-bound, bound]]))
     
     
-    x_indices = jnp.digitize(x, xedges)
-    y_indices = jnp.digitize(y, yedges)
+    x_indices = jnp.digitize(x, xedges) - 1
+    y_indices = jnp.digitize(y, yedges) - 1
     
     side_width = xedges[1] - xedges[0]
     
     alphas = x%side_width
     betas = y%side_width
     
-    big_alphas = alphas + side_width / 2    # these alphas are in the right half of their respective bin
-    big_betas = betas + side_width / 2      # these betas are in the top half of their respective bin
+    # big_alphas = (alphas + side_width / 2)%side_width    # these alphas are in the right half of their respective bin
+    # big_betas = (betas + side_width / 2)%side_width      # these betas are in the top half of their respective bin
     
-    a_s = jnp.minimum(alphas, big_alphas)
-    b_s = jnp.minimum(betas, big_betas)
+    # a_s = jnp.minimum(alphas, big_alphas)
+    # b_s = jnp.minimum(betas, big_betas)
     
-    one_minus_a_indices = x_indices - (1 + 2 * jnp.heaviside(alphas - side_width / 2, 0))
-    one_minus_b_indices = y_indices - (1 + 2 * jnp.heaviside(betas - side_width / 2, 0))
+    # big_alphas = alphas     # these alphas are in the right half of their respective bin
+    # big_betas = (betas + side_width / 2)%side_width      # these betas are in the top half of their respective bin
+    
+    a_s = jnp.minimum(alphas, side_width - alphas) + side_width / 2
+    b_s = jnp.minimum(betas, side_width - betas) + side_width / 2 
+    one_minus_a_indices = x_indices - 1 + 2 * jnp.heaviside(alphas - side_width / 2, 0)
+    one_minus_b_indices = y_indices - 1 + 2 * jnp.heaviside(betas - side_width / 2, 0)
+    
+    # one_minus_a_indices = x_indices - 1 + 2 * jnp.heaviside(alphas - side_width / 2, 0)
+    # one_minus_b_indices = y_indices - 1 + 2 * jnp.heaviside(betas - side_width / 2, 0)
+    one_minus_a_indices = one_minus_a_indices.astype(int)
+    one_minus_b_indices = one_minus_b_indices.astype(int)
     
     # now check the indices that are out of bounds
-    x_edge_check = jnp.heaviside(one_minus_a_indices, 1) * jnp.heaviside(len(x_indices) - one_minus_a_indices, 0)
-    y_edge_check = jnp.heaviside(one_minus_b_indices, 1) * jnp.heaviside(len(y_indices) - one_minus_b_indices, 0)
+    x_edge_check = jnp.heaviside(one_minus_a_indices, 1) * jnp.heaviside(im_size - one_minus_a_indices, 0)
+    y_edge_check = jnp.heaviside(one_minus_b_indices, 1) * jnp.heaviside(im_size - one_minus_b_indices, 0)
     
     x_edge_check = x_edge_check.astype(int)
     y_edge_check = y_edge_check.astype(int)
@@ -437,17 +447,27 @@ def smooth_histogram2d(particles, weights, stardata):
     # H[x_indices, y_indices * y_edge_check] += y_edge_check * vertical_quadrant
     # H[x_indices * x_edge_check, y_indices * y_edge_check] += x_edge_check * y_edge_check * corner_quadrant
     
-    # The below few lines rely fundamentally on the following line sourced from https://jax.readthedocs.io/en/latest/_autosummary/jax.numpy.ndarray.at.html:
+    # The below few lines rely fundamentally on the following line sourced from https://jax.readthedocs.io/en/latest/_autosummary/jax.numpy.ndarray.at.html :
     # Unlike NumPy in-place operations such as x[idx] += y, if multiple indices refer to the same location, all updates will be applied (NumPy would only apply the last update, rather than applying all updates.)
     
     H = jnp.zeros((im_size, im_size))
+    # H = H.at[x_indices, y_indices].add(main_quadrant)
+    # H = H.at[x_indices * x_edge_check, y_indices].add(x_edge_check * horizontal_quadrant)
+    # H = H.at[x_indices, y_indices * y_edge_check].add(y_edge_check * vertical_quadrant)
+    # H = H.at[x_indices * x_edge_check, y_indices * y_edge_check].add(x_edge_check * y_edge_check * corner_quadrant)
+    
+    # H = H.at[x_indices, y_indices].add(main_quadrant)
+    # H = H.at[one_minus_a_indices, y_indices].add(x_edge_check * horizontal_quadrant)
+    # H = H.at[x_indices, one_minus_b_indices].add(y_edge_check * vertical_quadrant)
+    # H = H.at[one_minus_a_indices, one_minus_b_indices].add(x_edge_check * y_edge_check * corner_quadrant)
+    
     H = H.at[x_indices, y_indices].add(main_quadrant)
-    H = H.at[x_indices * x_edge_check, y_indices].add(x_edge_check * horizontal_quadrant)
-    H = H.at[x_indices, y_indices * y_edge_check].add(y_edge_check * vertical_quadrant)
-    H = H.at[x_indices * x_edge_check, y_indices * y_edge_check].add(x_edge_check * y_edge_check * corner_quadrant)
+    H = H.at[one_minus_a_indices, y_indices].add(horizontal_quadrant)
+    H = H.at[x_indices, one_minus_b_indices].add(vertical_quadrant)
+    H = H.at[one_minus_a_indices, one_minus_b_indices].add(corner_quadrant)
     
     X, Y = jnp.meshgrid(xedges, yedges)
-    # H = H.T
+    H = H.T
     H /= jnp.max(H)
     
     H = jnp.minimum(H, jnp.ones((im_size, im_size)) * stardata['histmax'])